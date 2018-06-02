474a176ed67592f92ba33e9d0309514f65eb3439
David Hacker <dmhacker@protonmail.com>
2018-06-01 22:45:44 -0700

    Fix output parsing when GDB is in stdlib functions

--- src/gg.cpp
+++ src/gg.cpp
@@ -285,174 +285,181 @@ long GDB::get_source_list_size() {
   return std::stol(last_word); 
 }
 
-long GDB::get_source_line_number() {
-  std::string output = execute_and_read(GDB_WHERE);
-  std::string target_line = output.substr(output.find(':') + 1, output.size());
-  std::string target_word = target_line.substr(0, target_line.find('\n'));
-  return std::stol(target_word);
-}
+  long GDB::get_source_line_number() {
+    std::string output = execute_and_read(GDB_WHERE);
 
-bool GDBApp::OnInit() {
-  // Determine screen and application dimensions
-  long screen_x = wxSystemSettings::GetMetric(wxSYS_SCREEN_X);
-  long screen_y = wxSystemSettings::GetMetric(wxSYS_SCREEN_Y);
-  long frame_x = screen_x / 2;
-  long frame_y = screen_y / 2;
+    // Edge case: program can still be running but 
+    // entering stdlib functions does not return line numbers
+    if (output.find(':') == -1) {
+      return 0;
+    }
 
-  // Build command used to initialize application 
-  std::string args;
-  for (int i = 1; i < wxApp::argc; i++) {
-    args.append(wxApp::argv[i]);
-    if (i < wxApp::argc - 1) {
-      args.append(" ");
+    std::string target_line = output.substr(output.find(':') + 1, output.size());
+    std::string target_word = target_line.substr(0, target_line.find('\n'));
+    return std::stol(target_word);
+  }
+
+  bool GDBApp::OnInit() {
+    // Determine screen and application dimensions
+    long screen_x = wxSystemSettings::GetMetric(wxSYS_SCREEN_X);
+    long screen_y = wxSystemSettings::GetMetric(wxSYS_SCREEN_Y);
+    long frame_x = screen_x / 2;
+    long frame_y = screen_y / 2;
+
+    // Build command used to initialize application 
+    std::string args;
+    for (int i = 1; i < wxApp::argc; i++) {
+      args.append(wxApp::argv[i]);
+      if (i < wxApp::argc - 1) {
+        args.append(" ");
+      }
+    }
+
+    // Create main frame and display 
+    GDBFrame * frame = new GDBFrame(GG_FRAME_TITLE, wxApp::argv[0], args, 
+        wxPoint((screen_x - frame_x) / 2, (screen_y - frame_y) / 2), 
+        wxSize(frame_x, frame_y));
+    frame->Show(true);
+
+    // Set top window to be the frame
+    SetTopWindow(frame);
+
+    return true;
+  }
+
+  GDBFrame::GDBFrame(const wxString & title, 
+      const wxString & clcommand, const wxString & clargs,
+      const wxPoint & pos, const wxSize & size) :
+    wxFrame(NULL, wxID_ANY, title, pos, size), 
+    command(clcommand), args(clargs)
+  {
+    // File section in the menu bar
+    wxMenu * menuFile = new wxMenu();
+    menuFile->Append(wxID_EXIT);
+
+    // Help section in the menu bar
+    wxMenu * menuHelp = new wxMenu();
+    menuHelp->Append(wxID_ABOUT);
+
+    // Menu bar on the top 
+    wxMenuBar * menuBar = new wxMenuBar();
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar(menuBar);
+
+    // Status bar on the bottom
+    CreateStatusBar();
+    SetStatusText(GDB_STATUS_IDLE);
+
+    // Create notebook (tabbed pane)
+    wxNotebook * tabs = new wxNotebook(this, wxID_ANY);
+
+    // Create source code display 
+    sourcePanel = new GDBSourcePanel(tabs);
+    tabs->AddPage(sourcePanel, "Source");
+
+    // Create assembly code display
+    assemblyPanel = new GDBAssemblyPanel(tabs);
+    tabs->AddPage(assemblyPanel, "Assembly");
+  }
+
+  void GDBFrame::OnAbout(wxCommandEvent & event) {
+    // Display static information
+    const char * information = 
+      "\nVersion: v"
+      GG_VERSION
+      "\nAuthors: "
+      GG_AUTHORS
+      "\nLicense: "
+      GG_LICENSE;
+
+    // Display per-instance information 
+    std::string text(information);
+    text.append("\n\nCommand: ");
+    text.append(command.c_str());
+    text.append("\nArguments: ");
+    text.append(args.c_str());
+
+    // Show message box with the text
+    wxMessageBox(text, GG_ABOUT_TITLE, wxOK | wxICON_INFORMATION);
+  }
+
+  GDBSourcePanel::GDBSourcePanel(wxWindow * parent) :
+    wxPanel(parent, wxID_ANY) 
+  {
+    // Create main sizer
+    wxGridBagSizer * sizer = new wxGridBagSizer();
+    SetSizer(sizer);
+
+    // Style for future text boxes
+    long textCtrlStyle = wxTE_MULTILINE | wxTE_READONLY | wxTE_RICH | wxHSCROLL | wxVSCROLL;
+
+    // Create source code display and add to sizer
+    sourceCodeText = new wxTextCtrl(this, wxID_ANY, 
+        wxT(GDB_NO_SOURCE_CODE),
+        wxDefaultPosition, wxDefaultSize, textCtrlStyle);
+    sizer->Add(sourceCodeText, 
+        wxGBPosition(0, 0), wxGBSpan(2, 1), 
+        wxALL | wxEXPAND, 5);
+
+    // Create local variables display and add to sizer
+    localsText = new wxTextCtrl(this, wxID_ANY, 
+        wxT(GDB_NO_LOCALS),
+        wxDefaultPosition, wxDefaultSize, textCtrlStyle);
+    sizer->Add(localsText, 
+        wxGBPosition(0, 1), wxGBSpan(1, 1), 
+        wxALL | wxEXPAND, 5);
+
+    // Create formal parameters display and add to sizer
+    paramsText = new wxTextCtrl(this, wxID_ANY, 
+        wxT(GDB_NO_PARAMS),
+        wxDefaultPosition, wxDefaultSize, textCtrlStyle);
+    sizer->Add(paramsText, 
+        wxGBPosition(1, 1), wxGBSpan(1, 1), 
+        wxALL | wxEXPAND, 5);
+
+    // Specify sizer rows and columns that should be growable
+    for (int i = 0; i < 2; i++) {
+      sizer->AddGrowableRow(i, 1);
+      sizer->AddGrowableCol(i, 1);
     }
   }
 
-  // Create main frame and display 
-  GDBFrame * frame = new GDBFrame(GG_FRAME_TITLE, wxApp::argv[0], args, 
-      wxPoint((screen_x - frame_x) / 2, (screen_y - frame_y) / 2), 
-      wxSize(frame_x, frame_y));
-  frame->Show(true);
+  GDBAssemblyPanel::GDBAssemblyPanel(wxWindow * parent) :
+    wxPanel(parent, wxID_ANY) 
+  {
+    // Create sizer
+    wxBoxSizer * sizer = new wxBoxSizer(wxHORIZONTAL);
+    SetSizer(sizer);
 
-  // Set top window to be the frame
-  SetTopWindow(frame);
+    // Style for future text boxes
+    long textCtrlStyle = wxTE_MULTILINE | wxTE_READONLY | wxTE_RICH | wxHSCROLL | wxVSCROLL;
 
-  return true;
-}
+    // Create assembly code display and add to sizer
+    assemblyCodeText = new wxTextCtrl(this, wxID_ANY, 
+        wxT(GDB_NO_ASSEMBLY_CODE),
+        wxDefaultPosition, wxDefaultSize, textCtrlStyle);
+    sizer->Add(assemblyCodeText, 1, wxALL | wxEXPAND, 5);
 
-GDBFrame::GDBFrame(const wxString & title, 
-    const wxString & clcommand, const wxString & clargs,
-    const wxPoint & pos, const wxSize & size) :
-  wxFrame(NULL, wxID_ANY, title, pos, size), 
-  command(clcommand), args(clargs)
-{
-  // File section in the menu bar
-  wxMenu * menuFile = new wxMenu();
-  menuFile->Append(wxID_EXIT);
-
-  // Help section in the menu bar
-  wxMenu * menuHelp = new wxMenu();
-  menuHelp->Append(wxID_ABOUT);
-
-  // Menu bar on the top 
-  wxMenuBar * menuBar = new wxMenuBar();
-  menuBar->Append(menuFile, "&File");
-  menuBar->Append(menuHelp, "&Help");
-  SetMenuBar(menuBar);
-
-  // Status bar on the bottom
-  CreateStatusBar();
-  SetStatusText(GDB_STATUS_IDLE);
-
-  // Create notebook (tabbed pane)
-  wxNotebook * tabs = new wxNotebook(this, wxID_ANY);
-
-  // Create source code display 
-  sourcePanel = new GDBSourcePanel(tabs);
-  tabs->AddPage(sourcePanel, "Source");
-
-  // Create assembly code display
-  assemblyPanel = new GDBAssemblyPanel(tabs);
-  tabs->AddPage(assemblyPanel, "Assembly");
-}
-
-void GDBFrame::OnAbout(wxCommandEvent & event) {
-  // Display static information
-  const char * information = 
-    "\nVersion: v"
-    GG_VERSION
-    "\nAuthors: "
-    GG_AUTHORS
-    "\nLicense: "
-    GG_LICENSE;
-
-  // Display per-instance information 
-  std::string text(information);
-  text.append("\n\nCommand: ");
-  text.append(command.c_str());
-  text.append("\nArguments: ");
-  text.append(args.c_str());
-
-  // Show message box with the text
-  wxMessageBox(text, GG_ABOUT_TITLE, wxOK | wxICON_INFORMATION);
-}
-
-GDBSourcePanel::GDBSourcePanel(wxWindow * parent) :
-  wxPanel(parent, wxID_ANY) 
-{
-  // Create main sizer
-  wxGridBagSizer * sizer = new wxGridBagSizer();
-  SetSizer(sizer);
-
-  // Style for future text boxes
-  long textCtrlStyle = wxTE_MULTILINE | wxTE_READONLY | wxTE_RICH | wxHSCROLL | wxVSCROLL;
-
-  // Create source code display and add to sizer
-  sourceCodeText = new wxTextCtrl(this, wxID_ANY, 
-      wxT(GDB_NO_SOURCE_CODE),
-      wxDefaultPosition, wxDefaultSize, textCtrlStyle);
-  sizer->Add(sourceCodeText, 
-      wxGBPosition(0, 0), wxGBSpan(2, 1), 
-      wxALL | wxEXPAND, 5);
-
-  // Create local variables display and add to sizer
-  localsText = new wxTextCtrl(this, wxID_ANY, 
-      wxT(GDB_NO_LOCALS),
-      wxDefaultPosition, wxDefaultSize, textCtrlStyle);
-  sizer->Add(localsText, 
-      wxGBPosition(0, 1), wxGBSpan(1, 1), 
-      wxALL | wxEXPAND, 5);
-
-  // Create formal parameters display and add to sizer
-  paramsText = new wxTextCtrl(this, wxID_ANY, 
-      wxT(GDB_NO_PARAMS),
-      wxDefaultPosition, wxDefaultSize, textCtrlStyle);
-  sizer->Add(paramsText, 
-      wxGBPosition(1, 1), wxGBSpan(1, 1), 
-      wxALL | wxEXPAND, 5);
-
-  // Specify sizer rows and columns that should be growable
-  for (int i = 0; i < 2; i++) {
-    sizer->AddGrowableRow(i, 1);
-    sizer->AddGrowableCol(i, 1);
+    // Create registers display and add to sizer
+    registersText = new wxTextCtrl(this, wxID_ANY, 
+        wxT(GDB_NO_REGISTERS),
+        wxDefaultPosition, wxDefaultSize, textCtrlStyle);
+    sizer->Add(registersText, 1, wxALL | wxEXPAND, 5);
   }
-}
 
-GDBAssemblyPanel::GDBAssemblyPanel(wxWindow * parent) :
-  wxPanel(parent, wxID_ANY) 
-{
-   // Create sizer
-  wxBoxSizer * sizer = new wxBoxSizer(wxHORIZONTAL);
-  SetSizer(sizer);
+  void update_console_and_gui(GDB & gdb) {
+    // Read from GDB to populate buffer
+    gdb.read_until_prompt(std::cout, std::cerr, true);
 
-  // Style for future text boxes
-  long textCtrlStyle = wxTE_MULTILINE | wxTE_READONLY | wxTE_RICH | wxHSCROLL | wxVSCROLL;
+    // Queue events if gdb is alive and 
+    // application has been initialized on separate thread
+    if (gdb.is_alive() && wxTheApp) { // App will be null if wxEntry() hasn't been called
+      wxWindow * window = wxTheApp->GetTopWindow();
+      if (window) { // Window will be null if GDBApp::OnInit() hasn't been called
+        wxEvtHandler * handler = window->GetEventHandler();
 
-  // Create assembly code display and add to sizer
-  assemblyCodeText = new wxTextCtrl(this, wxID_ANY, 
-      wxT(GDB_NO_ASSEMBLY_CODE),
-      wxDefaultPosition, wxDefaultSize, textCtrlStyle);
-  sizer->Add(assemblyCodeText, 1, wxALL | wxEXPAND, 5);
-
-  // Create registers display and add to sizer
-  registersText = new wxTextCtrl(this, wxID_ANY, 
-      wxT(GDB_NO_REGISTERS),
-      wxDefaultPosition, wxDefaultSize, textCtrlStyle);
-  sizer->Add(registersText, 1, wxALL | wxEXPAND, 5);
-}
-
-void update_console_and_gui(GDB & gdb) {
-  // Read from GDB to populate buffer
-  gdb.read_until_prompt(std::cout, std::cerr, true);
-
-  // Queue events if gdb is alive and 
-  // application has been initialized on separate thread
-  if (gdb.is_alive() && wxTheApp) { // App will be null if wxEntry() hasn't been called
-    wxWindow * window = wxTheApp->GetTopWindow();
-    if (window) { // Window will be null if GDBApp::OnInit() hasn't been called
-      wxEvtHandler * handler = window->GetEventHandler();
-
-      // Update displays if we detect line numbers have changed
+        // Update displays if we detect line numbers have changed
       long line_number = gdb.is_running_program() ?
         gdb.get_source_line_number() : 0;
       long saved_line_number = gdb.get_saved_line_number();
